41a0ad7b4bbf6c3c694209c2ca91e5ab8f8fd236
toona <amanou.toona@gmail.com>
2022-08-10 00:02:18 +0900

    Impl: UnionFind

--- src/main.rs
+++ src/main.rs
@@ -31,16 +31,56 @@ impl Logger {
 #[cfg(not(feature = "local"))]
 impl Logger {
     // 何もしないメソッドの実装
+}
+
+struct Unionfind {
+    parents: Vec<i64>,
+}
+
+impl Unionfind {
+    fn new(n: usize) -> Self {
+        Unionfind {
+            parents: vec![-1; n], // 負値:: -size  正値:: parent を示す
+        }
     }
 
+    fn find(&mut self, x: usize) -> usize {
+        if self.parents[x] < 0 {
+            return x;
+        } else {
+            return self.find(x);
+        }
+    }
+
+    fn unitre(&mut self, x: usize, y: usize) {
+        let mut x_root: usize = self.find(x);
+        let mut y_root: usize = self.find(y);
+
+        if x_root == y_root {
+            return;
+        }
+        if self.parents[x_root] > self.parents[y_root] {
+            std::mem::swap(&mut x_root, &mut y_root)
+        }
+        self.parents[x_root] += self.parents[y_root];
+        self.parents[y_root] = x_root;
+    }
+}
+
+struct Input {}
+
+fn compute_score() {}
 
 fn main() {
-
     // 入力
     let mut s: String = String::new();
     std::io::stdin().read_line(&mut s).ok();
 
-    let v: Vec<i64> = s.trim().split_whitespace().map(|e| e.parse::<i64>().ok().unwrap()).collect();
+    let v: Vec<i64> = s
+        .trim()
+        .split_whitespace()
+        .map(|e| e.parse::<i64>().ok().unwrap())
+        .collect();
     let n: i64 = v[0];
     let k: i64 = v[1];
 
@@ -56,14 +96,10 @@ fn main() {
 
     for i in 0..n {
         let i: usize = i as usize;
-        for j in 0..n{
-            let j : usize = j as usize;
+        for j in 0..n {
+            let j: usize = j as usize;
             print!("{}", c[i][j])
         }
         print!("\n")
     }
-
-
-
-
 }
